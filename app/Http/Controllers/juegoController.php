920c972f28f8ccd0af6854264db17670c78a5130
LizzieZam <43660974+LizzieZam@users.noreply.github.com>
2019-05-28 18:43:21 -0500

    Update juegoController.php

--- app/Http/Controllers/juegoController.php
+++ app/Http/Controllers/juegoController.php
@@ -15,7 +15,7 @@ class juegoController extends Controller
 
     public function index(){
 		
-		return \App\juego::latest('id')->first();
+		return \App\juego::latest('id')->first()->get('puntos');
 		
 	}
 	public function show($id){
