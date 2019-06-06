b6a656e7633f2f7902236db668766356bca00562
LizzieZam <43660974+LizzieZam@users.noreply.github.com>
2019-06-05 23:05:11 -0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -21,14 +21,18 @@ Route::get('restaurante','restauranteController@index');
 Route::get('restaurante/{id}','restauranteController@show');
 Route::post('restaurante','restauranteController@store');
 Route::post('restaurante/{id}','restauranteController@update');
+Route::delete('restaurante/{id}','restauranteController@delete');
+
 Route::get('pedido','pedidoController@index');
 Route::get('pedido/{id}','pedidoController@show');
 Route::post('pedido','pedidoController@store');
 Route::post('pedido/{id}','pedidoController@update');
+Route::delete('pedido/{id}','pedidoController@delete');
+
 Route::get('plato','platoController@index');
 Route::get('plato/{id}','platoController@show');
 Route::post('plato','platoController@store');
 Route::post('plato/{id}','platoController@update');
-Route::get('juego','juegoController@index');
-Route::post('juego','juegoController@store');
+Route::delete('plato/{id}','platoController@delete');
+
 
