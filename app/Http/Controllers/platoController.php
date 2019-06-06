1b68290e6e27ac70f3efcb4c28cc0620063a7c5f
LizzieZam <43660974+LizzieZam@users.noreply.github.com>
2019-06-05 20:21:40 -0500

    Update platoController.php

--- app/Http/Controllers/platoController.php
+++ app/Http/Controllers/platoController.php
@@ -27,7 +27,7 @@ class platoController extends Controller
 	}
 	public function update(Request $request,$id){
         $row=\App\plato::findOrFail($id);
-        $row=$request->all();
+       print_r($request->all());
         var_dump($row);
         $row->save();
 		return $row;
