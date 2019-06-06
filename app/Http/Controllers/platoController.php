888a5ad9ea69767254ecad591d1b79537cc1ed1d
Elizabeth <lizzie.2907@gmail.com>
2019-06-06 18:50:52 -0500

    imagen

--- app/Http/Controllers/platoController.php
+++ app/Http/Controllers/platoController.php
@@ -28,7 +28,7 @@ class platoController extends Controller
 		$imgNombre=$imgNombre+1;
 		
 	
-        $sub_path = Storage::putFileAs("images", $request->file('imagen'),$imgNombre.'.jpg'); 
+        $sub_path = Storage::putFileAs(public_path("images"), $request->file('imagen'),$imgNombre.'.jpg'); 
 		$data['imagen']=  $sub_path ;
 		
 		$row= \App\plato::create($data);
