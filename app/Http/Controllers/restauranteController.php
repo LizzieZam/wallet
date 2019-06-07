5477cf97e5a33e223e8eb6ee1aae5ce39eac2706
Elizabeth <lizzie.2907@gmail.com>
2019-06-06 20:05:01 -0500

    imagen

--- app/Http/Controllers/restauranteController.php
+++ app/Http/Controllers/restauranteController.php
@@ -22,7 +22,13 @@ class restauranteController extends Controller
 		return \App\restaurante::find($id);
 	}
 	public function store(Request $request){
-        $data =$request->all();
+		$data =$request->all();
+		$imgNombre=\App\restaurante::latest('id')->get('id')->first()['id'];
+		$imgNombre="restaurante".($imgNombre+1). '.' . $request->file('imagen')->getClientOriginalExtension();
+		$imagen = Image::make($request->file('imagen'));
+		$imagen->save(public_path()."/images/" . $imgNombre, 100);
+		$data['imagen']= "/images/" . $imgNombre;
+
 		return \App\restaurante::create($data);
 		
 	}
