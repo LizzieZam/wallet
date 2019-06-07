1a6f33fdf3408ccf602e6a07eec27e24c51fdace
Elizabeth <lizzie.2907@gmail.com>
2019-06-06 19:09:40 -0500

    imagen

--- app/Http/Controllers/platoController.php
+++ app/Http/Controllers/platoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use App\Traits\UploadTrait;
 use App\plato;
 
 class platoController extends Controller
